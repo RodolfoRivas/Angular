51fc2c89ac38cb607b621cb41211f3e0ee28cad4
RodolfoRivas <111072192+RodolfoRivas@users.noreply.github.com>
2022-08-14 18:59:50 -0600

    Plantilla de la pagina de el gato-- INSERT --

--- src/app/cuadro/cuadro.component.ts
+++ src/app/cuadro/cuadro.component.ts
@@ -2,11 +2,24 @@ import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-cuadro',
-  template: `
-  <button>{{value}}</button>
+  template: `<style>
+  button {
+    background-color: red;
+    margin: 20 auto;
+    border: none;
+    color: white;
+    padding: 60px 60px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 42px;
+    object-scale: 1;
+  }
+</style>
+  <button >{{value}}</button>
 
   `,
-  styles: [
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'
+    
   ]
 })
 export class CuadroComponent {
@@ -22,4 +35,16 @@ esto va arriba lo de abajo
       {{rnd}}
     </p>
   }*/
+  /*<style>
+  button {
+    background-color: #4CAF50;  Green 
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+</style>*/
 }
